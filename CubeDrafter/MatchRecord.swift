381a218a51612de9339cf24bb40a35ecc048f258
Facu Rubio <facundorubio97@gmail.com>
2024-12-04 21:39:20 -0300

    First commit with the structs of the proyects

---
+++ CubeDrafter/MatchRecord.swift
@@ -0,0 +1,8 @@
+//
+//  MatchRecord.swift
+//  CubeDrafter
+//
+//  Created by facundo rubio on 04/12/2024.
+//
+
+import Foundation
---
+++ CubeDrafter/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  CubeDrafter
+//
+//  Created by facundo rubio on 04/12/2024.
+//
+
+import Foundation
