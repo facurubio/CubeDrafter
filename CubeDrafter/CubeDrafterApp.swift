8da46818d9fa9aec5adaede2478ef1892e72585e
Facu Rubio <facundorubio97@gmail.com>
2024-12-04 21:30:59 -0300

    Initial Commit

---
+++ CubeDrafter/CubeDrafterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeDrafterApp.swift
+//  CubeDrafter
+//
+//  Created by facundo rubio on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CubeDrafterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
